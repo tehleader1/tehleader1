738ba4b1e29bd75b7902d74683a762cb6e25683e
tehleader1 <agentanthony@supportrd.com>
2026-03-17 01:20:38 -0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,5 @@
-self.addEventListener("install", event => {
+self.addEventListener("install",e=>{
 self.skipWaiting()
 })
 
-self.addEventListener("fetch", event => {
-})
+self.addEventListener("fetch",e=>{})
